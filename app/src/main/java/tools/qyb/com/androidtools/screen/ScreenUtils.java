af3188767190d9a1c033b42166fa30d5084a23ad
qiaoyubo <qyb0010@hotmail.com>
2015-04-19 14:13:34 +0800

    first commit

---
+++ app/src/main/java/tools/qyb/com/androidtools/screen/ScreenUtils.java
@@ -0,0 +1,22 @@
+package tools.qyb.com.androidtools.screen;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+public class ScreenUtils {
+
+    public static int getScreenWidth(Context context) {
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics metrics = new DisplayMetrics();
+        manager.getDefaultDisplay().getMetrics(metrics);
+        return metrics.widthPixels;
+    }
+
+    public static int getScreenHeight(Context context) {
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics metrics = new DisplayMetrics();
+        manager.getDefaultDisplay().getMetrics(metrics);
+        return metrics.heightPixels;
+    }
+}
